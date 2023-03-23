565e707d870ce11fcc09a8fc48d3326824e4ec70
Mahdi <hassanim430@gmail.com>
2023-03-23 20:32:47 +0430

    adding styles for create post component

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -2,8 +2,32 @@ import React from 'react'
 
 function Create() {
   return (
-    <div>
-        <h1>Create post component</h1>
+    <div className="app-container">
+      <h1>Add a new post</h1>
+      <form>
+
+        <div className='form-group'>
+            <label htmlFor="title">Post title</label>
+            <input 
+                type="text"
+                className="form-control"
+                id="title"
+                name="title"
+                placeholder="Post title"
+            />
+        </div>
+
+        <div className='form-group'>
+            <label htmlFor="description">Post description</label>
+            <textarea className="form-control" 
+                      name="description"
+                      id="description"
+                      placeholder="Post description"></textarea>
+        </div>
+
+        <button>Submit</button>
+
+      </form>
     </div>
   )
 }
