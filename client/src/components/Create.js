269b1812ab10ddbff53c0dffcbb9528cae5570dc
Mahdi <hassanim430@gmail.com>
2023-03-24 14:34:48 +0430

    adding addtopost function for create post component

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,35 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import uniqid from "uniqid";
 
 function Create() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const addPost = (e) => {
+    e.preventDefault();
+    let post = {
+      title: title,
+      description: description,
+      postid: uniqid(),
+    };
+
+    console.log(post);
+  };
+
   return (
     <div className="app-container">
       <h1>Add a new post</h1>
       <form>
-
-        <div className='form-group'>
-            <label htmlFor="title">Post title</label>
-            <input 
-                type="text"
-                className="form-control"
-                id="title"
-                name="title"
-                placeholder="Post title"
-            />
+        <div className="form-group">
+          <label htmlFor="title">Post title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            placeholder="Post title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
 
-        <div className='form-group'>
-            <label htmlFor="description">Post description</label>
-            <textarea className="form-control" 
-                      name="description"
-                      id="description"
-                      placeholder="Post description"></textarea>
+        <div className="form-group">
+          <label htmlFor="description">Post description</label>
+          <textarea
+            className="form-control"
+            name="description"
+            id="description"
+            placeholder="Post description"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
         </div>
 
-        <button>Submit</button>
-
+        <button onClick={addPost}>Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Create
+export default Create;
